c9075a66ea4c9c94c88360ed7e766b8bd3a1f658
Norman Meier <norman@berty.tech>
2021-08-24 11:12:41 +0200

    allow to choose makefile

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -49232,9 +49232,11 @@ function run() {
                 return;
             }
             const ruleTarget = core.getInput(constants_1.Inputs.Rule);
-            const deps = child_process_1.default.execSync(`make -pn ${ruleTarget} 2>/dev/null | grep "${ruleTarget}: " | cut -d: -f2`).toString("utf-8").trim();
+            const makefile = core.getInput(constants_1.Inputs.Makefile) || "Makefile";
+            const deps = child_process_1.default.execSync(`make -f ${makefile} -pn ${ruleTarget} 2>/dev/null | grep "${ruleTarget}: " | cut -d: -f2`).toString("utf-8").trim();
             core.info("Deps: " + deps);
             // FIXME: use file names and acls
+            // FIXME: add makefile or even better, the rule definition, to hash 
             const hash = child_process_1.default.execSync(`cat ${deps} | shasum | cut -d' ' -f1`).toString("utf-8");
             const primaryKey = "mkache-" + core.getInput(constants_1.Inputs.Key, { required: true }) + "-" + hash;
             core.saveState(constants_1.State.CachePrimaryKey, primaryKey);
